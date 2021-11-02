bd8b1712cd16680b03a15c655a22490137212083
Tzekov <46439753+stanislavtz@users.noreply.github.com>
2021-11-02 08:42:42 +0200

    Add username and password valirators

--- src/models/User.js
+++ src/models/User.js
@@ -6,12 +6,16 @@ const { SALT_ROUNDS } = require('../utils/constants');
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: [true, 'Username is required'],
-        unique: true
+        required: [true, 'The username is required'],
+        unique: true,
+        validate: [/^([A-Z0-9]+)$/i, 'The username should consist only english letters and digits'],
+        minlength: [5, 'The username should be at least 5 characters long']
     },
     password: {
         type: String,
-        required: [true, 'Password is required']
+        required: [true, 'The password is required'], 
+        validate: [/^([A-Z0-9]+)$/i, 'The password should consist only english letters and digits'],
+        minlength: [5, 'The password should be at least 5 characters long']
     },
     courses: [
         {
